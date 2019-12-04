17dd3a876f492ac0b78fa1230140908a06eaabba
Sajan Joseph <sajan@csdav.com>
2019-12-04 12:38:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import Application from './src/components/';
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Application
+      />
+    );
+  }
+}
